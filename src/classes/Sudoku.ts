f13b84d0ce97f6a87674b86999119f206447cef5
Jorik PC <vanveenjorik@gmail.com>
2020-10-26 11:14:03 +0100

    More style enforcement

--- src/classes/Sudoku.ts
+++ src/classes/Sudoku.ts
@@ -1,303 +1,302 @@
-import Coordinate from "./Coordinate"
-import SudokuCell from "./SudokuCell"
+import SudokuCell from './SudokuCell'
 
 export default class Sudoku {
-    private rows: SudokuCell[][]
-
-    constructor(sudokuData: string) {
-        if (sudokuData.length !== 81) {
-            throw new Error("Invalid sudoku string")
-        }
-
-        const rows = []
-        for (let y = 0; y < 9; y++) {
-            const row: SudokuCell[] = []
-
-            for (let x = 0; x < 9; x++) {
-                const value = parseInt(sudokuData[y*9 + x])
-
-                const cell = new SudokuCell(
-                    x,
-                    y,
-                    value,
-                    this
-                )
-
-                row.push(cell)
-            }
-
-            rows.push(row)
-        }
-
-        this.rows = rows
-    }
-
-    public getAllRows() {
-        return this.rows
-    }
-
-    public getAllColumns() {
-        const rows = this.rows
-        const cols = []
-
-        for (let x = 0; x < 9; x++) {
-            const col = []
-            for (const row of rows) {
-                col.push(row[x])
-            }
-
-            cols.push(col)
-        }
-
-        return cols
-    }
-
-    public getAllRowValues() {
-        return this.getAllRows().map(row => {
-            return row.map(cell => {
-                return cell.getValue()
-            })
-        })
-    }
-
-    public getAllColumnValues() {
-        return this.getAllColumns().map(col => {
-            return col.map(cell => {
-                return cell.getValue()
-            })
-        })
-    }
-
-    public getColumn(x:number) {
-        return this.getAllColumns()[x]
-    }
-
-    public getRow(y:number) {
-        return this.rows[y]
-    }
-
-    public getCellFromCoord(x:number, y:number) {
-        return this.rows[y][x]
-    }
-
-    public getCellFromIndex(index:number) {
-        const yCoord = Math.floor(index / 9)
-        const xCoord = index - yCoord * 9
-
-        return this.getCellFromCoord(xCoord, yCoord)
-    }
-
-    public toString() {
-        let finalString = ""
-
-        for (const row of this.rows) {
-            for (const cell of row) {
-                finalString += cell.getValue()
-            }
-        }
-
-        return finalString
-    }
-
-    private getFirstEmptyCell() {
-        let index = 0
-
-        while (true) {
-            const cell = this.getCellFromIndex(index)
-
-            if (!cell.isClue()) {
-                return cell
-            }
-
-            index++
-        }
-    }
-
-    public display() {
-        const rows = this.rows
-        let finalString = ""
-
-        for (const row of rows) {
-            for (const cell of row) {
-                finalString += cell.getValue().toString() + " "
-            }
-
-            finalString += "\n"
-        }
-
-        console.log(finalString)
-    }
-
-    public toHTMLTable() {
-        let table = "<table>"
-
-        for (const row of this.rows) {
-            table += "<tr>"
-            for (const cell of row) {
-                table += `<td><input disabled value="${cell.getValue()}"></td>`
-            }
-            table += "</tr>"
-        }
-
-        table += "</table>"
-
-        return table
-    }
-
-    public isSolved() {
-        return !this.rows.flat().find(cell => {
-            return cell.getValue() === 0
-        })
-    }
-
-    public solveLoneSingles() {
-        // Gets all cells that have only one possibility and fills in that possibility, repeats until all cells have more than 1 possibility or the puzzle is solved
-        let solvedAllLoneSingles = false
-        let solvedAnyLoneSingle = false
-
-        while (!solvedAllLoneSingles) {
-            let solvedAtLeastOne = false
-
-            for (const row of this.rows) {
-                for (const cell of row) {
-                    if (cell.isClue()) continue 
-                    const candidates = cell.getAllCandidates()
-
-                    if (candidates.length === 1) {
-                        cell.setValue(candidates[0])
-                        cell.makeImmutable()
-                        solvedAtLeastOne = true
-                        solvedAnyLoneSingle = true
-                    }
-                }
-            }
-
-            if (!solvedAtLeastOne) {
-                solvedAllLoneSingles = true
-            }
-        }
-
-        return solvedAnyLoneSingle
-    }
-
-    public solveHiddenSingles() {
-        let solvedAllHiddenSingles = false
-        let solvedAnyHiddenSingle = false
-
-        while (!solvedAllHiddenSingles) {
-            let solvedAtLeastOne = false
-
-            for (const row of this.rows) {
-                cellIteration: for (const cell of row) {
-                    if (cell.isClue()) continue cellIteration
-                    const ownCandidates = cell.getAllCandidates()
-                    for (const ownCandidate of ownCandidates) {
-                        const row = cell.getRow()
-                        const col = cell.getColumn()
-                        const subgrid = cell.getSubgrid()
-
-                        const candidatesFromCell = (inputCell:SudokuCell) => {
-                            if (!inputCell.isClue() && inputCell !== cell) {
-                                return inputCell.getAllCandidates()
-                            }
-                        }
-
-                        const rowCandidates = row.map(candidatesFromCell).flat()
-                        const colCandidates = col.map(candidatesFromCell).flat()
-                        const subgridCandidates = subgrid.map(candidatesFromCell).flat()
-
-                        const rowHasCandidate = rowCandidates.includes(ownCandidate)
-                        const colHasCandidate = colCandidates.includes(ownCandidate)
-                        const subgridHasCandidate = subgridCandidates.includes(ownCandidate)
-
-                        if (!rowHasCandidate || !colHasCandidate || !subgridHasCandidate) {
-                            cell.setValue(ownCandidate)
-                            cell.makeImmutable()
-                            console.log("Solved hidden single")
-                            solvedAtLeastOne = true
-                            solvedAnyHiddenSingle = true
-                        }
-                    }
-                }
-            }
-
-            if (!solvedAtLeastOne) {
-                solvedAllHiddenSingles = true
-            }
-        }
-
-        return solvedAnyHiddenSingle
-    }
-
-    public solveByBacktracking() {
-        let foundSolution = false
-        let currentCell = this.getFirstEmptyCell()
-
-        const nextCell = () => {
-            while (true) {
-                if (currentCell.getIndex() === 80) {
-                    foundSolution = true
-                    return false
-                }
-
-                currentCell = this.getCellFromIndex(currentCell.getIndex() + 1)
-
-                if (!currentCell.isClue()) {
-                    return true
-                }
-            }
-        }
-
-        const previousCell = () => {
-            currentCell.setValue(0)
-            while (true) {
-                currentCell = this.getCellFromIndex(currentCell.getIndex() - 1)
-
-                if (!currentCell.isClue()) {
-                    return true
-                }
-            }
-        }
-
-
-        while (!foundSolution) {
-            const newValue = currentCell.incrementValue()
-            const isNewValueValid = currentCell.testCurrentValue()
-
-            if (newValue > 9) {
-                previousCell()
-                continue
-            }
-
-            if (isNewValueValid && newValue !== 0) {
-                nextCell()
-            }
-            
-        }
-
-        console.log("solved")
-    }
-
-    public solve() {
-        const timeMeasureStart = performance.now()
-
-        while (true) {
-            const foundLoneSingle = this.solveLoneSingles()
-            const foundHiddenSingle = this.solveHiddenSingles()
-
-            if (!foundLoneSingle && !foundHiddenSingle) {
-                break
-            }
-        }
-
-        if (!this.isSolved()) {
-            console.log("Started backtracking")
-            this.display()
-            this.solveByBacktracking()
-        }
-
-        const timeMeasureEnd = performance.now()
-        const time = timeMeasureEnd - timeMeasureStart
-        console.log(`Solved in ${time}ms`)
-    }
+	private rows: SudokuCell[][]
+
+	constructor (sudokuData: string) {
+		if (sudokuData.length !== 81) {
+			throw new Error('Invalid sudoku string')
+		}
+
+		const rows = []
+		for (let y = 0; y < 9; y++) {
+			const row: SudokuCell[] = []
+
+			for (let x = 0; x < 9; x++) {
+				const value = parseInt(sudokuData[y * 9 + x])
+
+				const cell = new SudokuCell(
+					x,
+					y,
+					value,
+					this
+				)
+
+				row.push(cell)
+			}
+
+			rows.push(row)
+		}
+
+		this.rows = rows
+	}
+
+	public getAllRows () {
+		return this.rows
+	}
+
+	public getAllColumns () {
+		const rows = this.rows
+		const cols = []
+
+		for (let x = 0; x < 9; x++) {
+			const col = []
+			for (const row of rows) {
+				col.push(row[x])
+			}
+
+			cols.push(col)
+		}
+
+		return cols
+	}
+
+	public getAllRowValues () {
+		return this.getAllRows().map(row => {
+			return row.map(cell => {
+				return cell.getValue()
+			})
+		})
+	}
+
+	public getAllColumnValues () {
+		return this.getAllColumns().map(col => {
+			return col.map(cell => {
+				return cell.getValue()
+			})
+		})
+	}
+
+	public getColumn (x: number) {
+		return this.getAllColumns()[x]
+	}
+
+	public getRow (y: number) {
+		return this.rows[y]
+	}
+
+	public getCellFromCoord (x: number, y: number) {
+		return this.rows[y][x]
+	}
+
+	public getCellFromIndex (index: number) {
+		const yCoord = Math.floor(index / 9)
+		const xCoord = index - yCoord * 9
+
+		return this.getCellFromCoord(xCoord, yCoord)
+	}
+
+	public toString () {
+		let finalString = ''
+
+		for (const row of this.rows) {
+			for (const cell of row) {
+				finalString += cell.getValue()
+			}
+		}
+
+		return finalString
+	}
+
+	private getFirstEmptyCell () {
+		let index = 0
+
+		while (true) {
+			const cell = this.getCellFromIndex(index)
+
+			if (!cell.isClue()) {
+				return cell
+			}
+
+			index++
+		}
+	}
+
+	public display () {
+		const rows = this.rows
+		let finalString = ''
+
+		for (const row of rows) {
+			for (const cell of row) {
+				finalString += cell.getValue().toString() + ' '
+			}
+
+			finalString += '\n'
+		}
+
+		console.log(finalString)
+	}
+
+	public toHTMLTable () {
+		let table = '<table>'
+
+		for (const row of this.rows) {
+			table += '<tr>'
+			for (const cell of row) {
+				table += `<td><input disabled value="${cell.getValue()}"></td>`
+			}
+			table += '</tr>'
+		}
+
+		table += '</table>'
+
+		return table
+	}
+
+	public isSolved () {
+		return !this.rows.flat().find(cell => {
+			return cell.getValue() === 0
+		})
+	}
+
+	public solveLoneSingles () {
+		// Gets all cells that have only one possibility and fills in that possibility, repeats until all cells have more than 1 possibility or the puzzle is solved
+		let solvedAllLoneSingles = false
+		let solvedAnyLoneSingle = false
+
+		while (!solvedAllLoneSingles) {
+			let solvedAtLeastOne = false
+
+			for (const row of this.rows) {
+				for (const cell of row) {
+					if (cell.isClue()) continue
+					const candidates = cell.getAllCandidates()
+
+					if (candidates.length === 1) {
+						cell.setValue(candidates[0])
+						cell.makeImmutable()
+						solvedAtLeastOne = true
+						solvedAnyLoneSingle = true
+					}
+				}
+			}
+
+			if (!solvedAtLeastOne) {
+				solvedAllLoneSingles = true
+			}
+		}
+
+		return solvedAnyLoneSingle
+	}
+
+	public solveHiddenSingles () {
+		let solvedAllHiddenSingles = false
+		let solvedAnyHiddenSingle = false
+
+		while (!solvedAllHiddenSingles) {
+			let solvedAtLeastOne = false
+
+			for (const row of this.rows) {
+				for (const cell of row) {
+					if (cell.isClue()) continue
+					const ownCandidates = cell.getAllCandidates()
+					for (const ownCandidate of ownCandidates) {
+						const row = cell.getRow()
+						const col = cell.getColumn()
+						const subgrid = cell.getSubgrid()
+
+						const candidatesFromCell = (inputCell: SudokuCell) => {
+							if (!inputCell.isClue() && inputCell !== cell) {
+								return inputCell.getAllCandidates()
+							}
+						}
+
+						const rowCandidates = row.map(candidatesFromCell).flat()
+						const colCandidates = col.map(candidatesFromCell).flat()
+						const subgridCandidates = subgrid.map(candidatesFromCell).flat()
+
+						const rowHasCandidate = rowCandidates.includes(ownCandidate)
+						const colHasCandidate = colCandidates.includes(ownCandidate)
+						const subgridHasCandidate = subgridCandidates.includes(ownCandidate)
+
+						if (!rowHasCandidate || !colHasCandidate || !subgridHasCandidate) {
+							cell.setValue(ownCandidate)
+							cell.makeImmutable()
+							console.log('Solved hidden single')
+							solvedAtLeastOne = true
+							solvedAnyHiddenSingle = true
+						}
+					}
+				}
+			}
+
+			if (!solvedAtLeastOne) {
+				solvedAllHiddenSingles = true
+			}
+		}
+
+		return solvedAnyHiddenSingle
+	}
+
+	public solveByBacktracking () {
+		let foundSolution = false
+		let currentCell = this.getFirstEmptyCell()
+
+		const nextCell = () => {
+			while (true) {
+				if (currentCell.getIndex() === 80) {
+					foundSolution = true
+					return false
+				}
+
+				currentCell = this.getCellFromIndex(currentCell.getIndex() + 1)
+
+				if (!currentCell.isClue()) {
+					return true
+				}
+			}
+		}
+
+		const previousCell = () => {
+			currentCell.setValue(0)
+			while (true) {
+				currentCell = this.getCellFromIndex(currentCell.getIndex() - 1)
+
+				if (!currentCell.isClue()) {
+					return true
+				}
+			}
+		}
+
+		// Disable because foundSolution is modified in the functions
+		// eslint-disable-next-line no-unmodified-loop-condition
+		while (!foundSolution) {
+			const newValue = currentCell.incrementValue()
+			const isNewValueValid = currentCell.testCurrentValue()
+
+			if (newValue > 9) {
+				previousCell()
+				continue
+			}
+
+			if (isNewValueValid && newValue !== 0) {
+				nextCell()
+			}
+		}
+
+		console.log('solved')
+	}
+
+	public solve () {
+		const timeMeasureStart = performance.now()
+
+		while (true) {
+			const foundLoneSingle = this.solveLoneSingles()
+			const foundHiddenSingle = this.solveHiddenSingles()
+
+			if (!foundLoneSingle && !foundHiddenSingle) {
+				break
+			}
+		}
+
+		if (!this.isSolved()) {
+			console.log('Started backtracking')
+			this.display()
+			this.solveByBacktracking()
+		}
+
+		const timeMeasureEnd = performance.now()
+		const time = timeMeasureEnd - timeMeasureStart
+		console.log(`Solved in ${time}ms`)
+	}
 }
--- src/index.ts
+++ src/index.ts
@@ -3,13 +3,13 @@ import Sudoku from './classes/Sudoku'
 type axis = 'x' | 'y'
 
 function main () {
-  const sudoku = new Sudoku('000000900090083450040500280000265300000000000006714000074002030083150040002000000')
+	const sudoku = new Sudoku('000000900090083450040500280000265300000000000006714000074002030083150040002000000')
 
-  // Lost hele sudoku op, heeft nog geen error handling lmao
-  sudoku.solveByBacktracking()
+	// Lost hele sudoku op, heeft nog geen error handling lmao
+	sudoku.solveByBacktracking()
 
-  // Dumpt de hele sudoku in de console
-  sudoku.display()
+	// Dumpt de hele sudoku in de console
+	sudoku.display()
 }
 
 main()
@@ -20,122 +20,122 @@ const allCellInputs = document.querySelectorAll('.sudoku-cell-input')
 // let cellY: number
 
 function clickedCell (event: Event) {
-  const cell = event.target
-  if (!(cell instanceof HTMLInputElement)) return
+	const cell = event.target
+	if (!(cell instanceof HTMLInputElement)) return
 
-  cell.style.backgroundColor = 'rgba(0, 0, 0, 0.07)'
+	cell.style.backgroundColor = 'rgba(0, 0, 0, 0.07)'
 
 //   cellX = parseInt(cell.value.charAt(1)) // x coord van cell, oftewel de 2e character
 //   cellY = parseInt(cell.value.charAt(0)) // y coord
 }
 
 function changeInput (event: InputEvent) {
-  const cell = event.target
+	const cell = event.target
 
-  if (!(cell instanceof HTMLInputElement)) return
-  const number = parseInt(event.data)
+	if (!(cell instanceof HTMLInputElement)) return
+	const number = parseInt(event.data)
 
-  if ((number > 9 || number < 1) || number.toString() === 'NaN') {
-    cell.value = ''
-  } else {
-    cell.value = number.toString()
-  }
+	if ((number > 9 || number < 1) || number.toString() === 'NaN') {
+		cell.value = ''
+	} else {
+		cell.value = number.toString()
+	}
 
-  // alert(inputSudoku.getRow(cellY - 1));
+	// alert(inputSudoku.getRow(cellY - 1));
 }
 
 function lostFocus (event: Event) {
-  const cell = event.target
-  if (!(cell instanceof HTMLInputElement)) return
-  cell.style.backgroundColor = 'rgb(255, 255, 255)'
+	const cell = event.target
+	if (!(cell instanceof HTMLInputElement)) return
+	cell.style.backgroundColor = 'rgb(255, 255, 255)'
 }
 
 allCellInputs.forEach(cellInput => {
-  cellInput.addEventListener('focus', clickedCell)
-  cellInput.addEventListener('input', changeInput)
-  cellInput.addEventListener('blur', lostFocus)
+	cellInput.addEventListener('focus', clickedCell)
+	cellInput.addEventListener('input', changeInput)
+	cellInput.addEventListener('blur', lostFocus)
 })
 
 function moveFocus (axis: axis, moveDirection: number) {
-  const currentlyFocusedElement = document.activeElement
-  if (currentlyFocusedElement.className !== 'sudoku-cell-input') return
-  if (!(currentlyFocusedElement instanceof HTMLInputElement)) return
+	const currentlyFocusedElement = document.activeElement
+	if (currentlyFocusedElement.className !== 'sudoku-cell-input') return
+	if (!(currentlyFocusedElement instanceof HTMLInputElement)) return
 
-  if (axis === 'x') {
-    const newCoordinate = parseInt(currentlyFocusedElement.id.charAt(0)) + moveDirection
-    const idToFocus = newCoordinate.toString() + currentlyFocusedElement.id.charAt(1)
-    const elemToFocus = document.getElementById(idToFocus)
+	if (axis === 'x') {
+		const newCoordinate = parseInt(currentlyFocusedElement.id.charAt(0)) + moveDirection
+		const idToFocus = newCoordinate.toString() + currentlyFocusedElement.id.charAt(1)
+		const elemToFocus = document.getElementById(idToFocus)
 
-    if (elemToFocus instanceof HTMLInputElement) {
-      elemToFocus.focus()
-    }
-  } else if (axis === 'y') {
-    const newCoordinate = parseInt(currentlyFocusedElement.id.charAt(1)) + moveDirection
-    const idToFocus = currentlyFocusedElement.id.charAt(0) + newCoordinate.toString()
-    const elemToFocus = document.getElementById(idToFocus)
+		if (elemToFocus instanceof HTMLInputElement) {
+			elemToFocus.focus()
+		}
+	} else if (axis === 'y') {
+		const newCoordinate = parseInt(currentlyFocusedElement.id.charAt(1)) + moveDirection
+		const idToFocus = currentlyFocusedElement.id.charAt(0) + newCoordinate.toString()
+		const elemToFocus = document.getElementById(idToFocus)
 
-    if (elemToFocus instanceof HTMLInputElement) {
-      elemToFocus.focus()
-    }
-  }
+		if (elemToFocus instanceof HTMLInputElement) {
+			elemToFocus.focus()
+		}
+	}
 }
 
 function onKeyDown (event: KeyboardEvent) {
-  const key = event.key
-  switch (key) {
-    case 'ArrowLeft':
-      moveFocus('y', -1)
-      break
-    case 'ArrowRight':
-      moveFocus('y', 1)
-      break
-    case 'ArrowUp':
-      event.preventDefault()
-      moveFocus('x', -1)
-      break
-    case 'ArrowDown':
-      event.preventDefault()
-      moveFocus('x', 1)
-      break
-  }
+	const key = event.key
+	switch (key) {
+	case 'ArrowLeft':
+		moveFocus('y', -1)
+		break
+	case 'ArrowRight':
+		moveFocus('y', 1)
+		break
+	case 'ArrowUp':
+		event.preventDefault()
+		moveFocus('x', -1)
+		break
+	case 'ArrowDown':
+		event.preventDefault()
+		moveFocus('x', 1)
+		break
+	}
 }
 
 document.addEventListener('keydown', onKeyDown)
 
 // 000000900090083450040500280000265300000000000006714000074002030083150040002000000
 function getSudokuString () {
-  var cellsA = document.querySelectorAll('#inputSudoku td > input')
-  var sudokuString = ''
+	var cellsA = document.querySelectorAll('#inputSudoku td > input')
+	var sudokuString = ''
 
-  cellsA.forEach(cell => {
-    if (!(cell instanceof HTMLInputElement)) return
-    if (cell.value === '') {
-      sudokuString += '0'
-      return
-    }
-    sudokuString += cell.value
-  })
+	cellsA.forEach(cell => {
+		if (!(cell instanceof HTMLInputElement)) return
+		if (cell.value === '') {
+			sudokuString += '0'
+			return
+		}
+		sudokuString += cell.value
+	})
 
-  console.log(sudokuString)
-  return sudokuString
+	console.log(sudokuString)
+	return sudokuString
 }
 
 document.getElementById('knop').addEventListener('click', () => {
-  // const sudoku1 = new Sudoku(getSudokuString())
-  // const sudoku2 = new Sudoku(getSudokuString())
+	// const sudoku1 = new Sudoku(getSudokuString())
+	// const sudoku2 = new Sudoku(getSudokuString())
 
-  const sudoku = new Sudoku(getSudokuString())
-  // const cell = sudoku.getCellFromCoord(2, 2)
-  // console.log(cell.getAllCandidates().includes(9))
-  sudoku.display()
-  sudoku.solve()
-  sudoku.display()
+	const sudoku = new Sudoku(getSudokuString())
+	// const cell = sudoku.getCellFromCoord(2, 2)
+	// console.log(cell.getAllCandidates().includes(9))
+	sudoku.display()
+	sudoku.solve()
+	sudoku.display()
 
-  document.getElementById('tableOutput').innerHTML = sudoku.toHTMLTable()
+	document.getElementById('tableOutput').innerHTML = sudoku.toHTMLTable()
 
-  // sudoku.display()
-  // sudoku.solveByBacktracking()
+	// sudoku.display()
+	// sudoku.solveByBacktracking()
 
-  // console.log(sudoku.toString())
-  // sudoku.getRow(1)
+	// console.log(sudoku.toString())
+	// sudoku.getRow(1)
 })
